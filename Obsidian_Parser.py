6a0a4fff2eff9f7c3c3eaecc98bd8ded4327442c
Briana D. Palmer <130857923+brianatia@users.noreply.github.com>
2026-04-10 21:06:30 -0500

    Add authorship comments to Obsidian_Parser.py
    Added authorship information at the top of the file.

--- Obsidian_Parser.py
+++ Obsidian_Parser.py
@@ -1,3 +1,5 @@
+#Briana Palmer -2200260, Giovaunni Sewell – 2207365, Tarik Savage – 2105093, Dominic Williams - 2105358
+
 import ply.yacc as yacc
 from Obsidian_Lexer import tokens
 from Error import Error
